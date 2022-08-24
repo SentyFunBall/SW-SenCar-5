da5c5df037d74970f91b779dc25679eaf3817e99
Senty <haydenbugatti@gmail.com>
2022-08-23 23:08:18 -0700

    framework for door screen

--- dashicons.lua
+++ dashicons.lua
@@ -20,7 +20,7 @@ do
     ---@type Simulator -- Set properties and screen sizes here - will run once when the script is loaded
     simulator = simulator
     simulator:setScreen(1, "3x1")
-    simulator:setProperty("ExampleNumberProperty", 123)
+    simulator:setProperty("Theme", 1)
 
     -- Runs every tick just before onTick; allows you to simulate the inputs changing
     ---@param simulator Simulator Use simulator:<function>() to set inputs etc.
@@ -31,6 +31,7 @@ do
         simulator:setInputBool(1, simulator:getIsToggled(1))
         simulator:setInputBool(2, simulator:getIsToggled(2))
         simulator:setInputBool(3, simulator:getIsToggled(3))
+        simulator:setInputBool(4, true)
     end;
 end
 ---@endsection
@@ -42,13 +43,27 @@ end
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
 
 require("LifeBoatAPI")
+
+_colors = {
+    {{47,51,78}, {86,67,143}, {128,95,164}}, --sencar 5 in the micro
+    {{17, 15, 107}, {22, 121, 196}, {48, 208, 217}} --blue
+}
+
 function onTick()
     leftBlinker = input.getBool(1)
     rightBlinker = input.getBool(2)
     cruise = input.getBool(3)
+    fl = input.getBool(4)
+    fr = input.getBool(5)
+    rl = input.getBool(6)
+    rr = input.getBool(7)
+
+    theme = property.getNumber("Theme")
 end
 
 function onDraw()
+    local _ = _colors[theme]
+    
     if leftBlinker then
         c(45,201,55)
         screen.drawTriangleF(60,24,60,30,69,27)
@@ -66,6 +81,15 @@ function onDraw()
         screen.drawLine(38,27,38,30)
         screen.drawRectF(38,25,1,1)
         screen.drawLine(39,27,41,29)
+    end
+
+    if fl or fr or rl or rr then
+        c(_[2][1], _[2][2], _[2][3])
+        screen.drawRectF(35,3,26,26)
+        screen.drawLine(36,2,60,2)
+        screen.drawLine(61,4,61,28)
+        screen.drawLine(36,29,60,29)
+        screen.drawLine(34,4,34,28)
     end
 end
 
