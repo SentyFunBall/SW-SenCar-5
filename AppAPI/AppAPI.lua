d031293545772aa34c819b9461adaff7a760bb4e
Senty <haydenbugatti@gmail.com>
2022-08-24 21:01:13 -0700

    added appapi, widgetapi, and configured it in widgetdisplay

--- AppAPI/AppAPI.lua
+++ AppAPI/AppAPI.lua
@@ -1,7 +1,6 @@
---widgetdisplay
 -- Author: SentyFunBall
 -- GitHub: https://github.com/SentyFunBall
--- Workshop: <WorkshopLink>
+-- Workshop: 
 
 --Code by STCorp. Do not reuse.--
 --- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
@@ -19,8 +18,8 @@
 do
     ---@type Simulator -- Set properties and screen sizes here - will run once when the script is loaded
     simulator = simulator
-    simulator:setScreen(1, "3x1")
-    simulator:setProperty("Theme", 1)
+    simulator:setScreen(1, "3x3")
+    simulator:setProperty("ExampleNumberProperty", 123)
 
     -- Runs every tick just before onTick; allows you to simulate the inputs changing
     ---@param simulator Simulator Use simulator:<function>() to set inputs etc.
@@ -36,47 +35,26 @@ do
         simulator:setInputNumber(4, screenConnection.touchY)
 
         -- NEW! button/slider options from the UI
-        simulator:setInputBool(1, true)
+        simulator:setInputBool(31, simulator:getIsClicked(1))       -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
+        simulator:setInputNumber(31, simulator:getSlider(1))        -- set input 31 to the value of slider 1
+
+        simulator:setInputBool(32, simulator:getIsToggled(2))       -- make button 2 a toggle, for input.getBool(32)
+        simulator:setInputNumber(32, simulator:getSlider(2) * 50)   -- set input 32 to the value from slider 2 * 50
     end;
 end
 ---@endsection
+
 
 --[====[ IN-GAME CODE ]====]
 
 -- try require("Folder.Filename") to include code from another file in this, so you can store code in libraries
 -- the "LifeBoatAPI" is included by default in /_build/libs/ - you can use require("LifeBoatAPI") to get this, and use all the LifeBoatAPI.<functions>!
-require("LifeBoatAPI")
-require("WidgetAPI.WidgetAPI")
 
-_colors = {
-    {{47,51,78}, {86,67,143}, {128,95,164}}, --sencar 5 in the micro
-    {{17, 15, 107}, {22, 121, 196}, {48, 208, 217}} --blue
-}
-
+ticks = 0
 function onTick()
-    acc = input.getBool(1)
-    theme = property.getNumber("Theme")
+    ticks = ticks + 1
 end
 
 function onDraw()
-    local _ = _colors[theme]
-    if acc then
-        for i = 1, 97 do
-            c(lerp(_[1][1], _[2][1], i/96), lerp(_[1][2], _[2][2], i/96), lerp(_[1][3], _[2][3], i/96))
-            screen.drawLine(i-1, 0, i-1, 32)
-        end
-
-        WidgetAPI.draw(1, true, 1)
-    end
-end
-
-function c(...) local _={...}
-    for i,v in pairs(_) do
-     _[i]=_[i]^2.2/255^2.2*_[i]
-    end
-    screen.setColor(table.unpack(_))
-end
-
-function lerp(v0,v1,t)
-    return v1*t+v0*(1-t)
+    screen.drawCircle(16,16,5)
 end
---
+++ WidgetAPI/WidgetAPI.lua
@@ -0,0 +1,15 @@
+-- Author: SentyFunBall
+-- GitHub: https://github.com/SentyFunBall
+-- Workshop: 
+
+--Code by STCorp. Do not reuse.--
+--- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
+--- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
+
+require("LifeBoatAPI")
+
+WidgetAPI = {
+    draw = function(slot, large, widget)
+        screen.drawCircle(5, 5, 5)
+    end
+}
