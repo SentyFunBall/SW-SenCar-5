8c6a173397f7816e8c2a215a1688511ca5efdfb1
Senty <haydenbugatti@gmail.com>
2022-08-25 23:29:40 -0700

    added APIs in special folder

---
+++ APIs/WidgetAPI.lua
@@ -0,0 +1,124 @@
+-- Author: SentyFunBall
+-- GitHub: https://github.com/SentyFunBall
+-- Workshop: 
+
+--Code by STCorp. Do not reuse.--
+--- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
+--- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
+
+require("LifeBoatAPI")
+
+active= {slot1 = {id = -1, large = false}, slot2 = {id = -1, large = false}, slot3 = {id = -1, large = false}}
+
+
+WidgetAPI = {
+    draw = function (slot, large, widget, color)
+        widget.drawn = false --by default, we assume it didnt draw
+        c(color[1], color[2], color[3], 240)
+        if slot == 1 then
+            if active.slot1.id == widget.id then --if the widget is assigned to this slot
+                if large then --large widget, draw the large style
+                    drawRoundedRect((slot-1)*32+2, 2, 58, 27)
+                    widget.drawn = true
+                else --dont draw the large style
+                    drawRoundedRect((slot-1)*32+2, 2, 27, 27)
+                    widget.drawn = true
+                end
+            else
+                if active.slot1.id == -1 then
+                    if large then
+                        active.slot1.id = widget.id
+                        active.slot1.large = true
+                        active.slot2.id = widget.id
+                    else
+                        active.slot1.id = widget.id
+                        active.slot1.large = false
+                    end
+                else
+                    widget.drawn = false
+                end
+            end
+        elseif slot == 2 then
+            if active.slot2.id == widget.id and not active.slot1.large then
+                if large then 
+                    drawRoundedRect((slot-1)*32+2, 2, 59, 27)
+                    widget.drawn = true
+                else
+                    drawRoundedRect((slot-1)*32+2, 2, 27, 27)
+                    widget.drawn = true
+                end
+            else
+                if active.slot2.id == -1 then
+                    if large then
+                        active.slot2.id = widget.id
+                        active.slot2.large = true
+                        active.slot3.id = widget.id
+                    else
+                        active.slot2.id = widget.id
+                        active.slot2.large = false
+                    end
+                else
+                    widget.drawn = false
+                end
+            end
+        elseif slot == 3 then
+            if active.slot3.id == widget.id and not active.slot2.large then
+                if large then
+                    drawRoundedRect((slot-1)*32+2, 2, 27, 27)
+                        screen.setColor(255,255,255)
+                        screen.drawRectF(76,13,1,1)
+                        screen.drawRectF(76,18,1,1)
+                        screen.drawRectF(83,13,1,1)
+                        screen.drawRectF(83,18,1,1)
+                        screen.drawLine(82,14,82.25,17.25)
+                else
+                    drawRoundedRect((slot-1)*32+2, 2, 27, 27) --THERE IS NO LARGE
+                    widget.drawn = true
+                end
+            else
+                if active.slot3.id == -1 then
+                    if large then
+                        active.slot3.id = widget.id
+                        widget.drawn = true
+                        return widget
+                    else
+                        active.slot3.id = widget.id
+                        active.slot3.large = false
+                    end
+                else
+                    widget.drawn = false
+                end
+            end
+        end
+        return widget
+    end,
+
+    remove = function (slot, id)
+        local didRemove = false
+        if slot == 1 then
+            if id == active.slot1.id then
+                active.slot1.id = -1
+                didRemove = true
+            end
+        elseif slot == 2 then
+            if id == active.slot2.id then
+                active.slot2.id = -1
+                didRemove = true
+            end
+        elseif slot == 3 then
+            if id == active.slot2.id then
+                active.slot2.id = -1
+                didRemove = true
+            end
+        end
+        return didRemove
+    end
+}
+
+function drawRoundedRect(x, y, w, h)
+    screen.drawRectF(x+1, y+1, w-1, h-1) --body
+    screen.drawLine(x+2, y, x+w-1, y) --top
+    screen.drawLine(x, y+2, x, y+h-1) --left
+    screen.drawLine(x+w, y+2, x+w, y+h-1) --right
+    screen.drawLine(x+2, y+h, x+w-1, y+h) --bottom
+end
