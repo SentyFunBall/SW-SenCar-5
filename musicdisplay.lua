70816445609e5acdba140647fa400b56aeb36c66
Senty <haydenbugatti@gmail.com>
2022-08-27 16:21:06 -0700

    music display checks if extras micro is connected; err if not

--- musicdisplay.lua
+++ musicdisplay.lua
@@ -66,6 +66,7 @@ function onTick()
     channel = math.ceil(input.getNumber(1))
     signalStrength = input.getNumber(2)
     isPlayingMusic = input.getBool(2)
+    connected = input.getBool(4)
 
     isPressed = input.getBool(3)
     -- channel buttons
@@ -92,121 +93,132 @@ function onDraw()
             screen.drawLine(i-1, 0, i-1, 32)
         end
 
-        --background
-        if not isPlayingMusic then
+        if connected then
+            --background
+            if not isPlayingMusic then
+                c(_[1][1], _[1][2], _[1][3],250) --i love tables
+                screen.drawRectF(3,3,26,26)
+                screen.drawLine(4,2,28,2)
+                screen.drawLine(29,4,29,28)
+                screen.drawLine(4,29,28,29)
+                screen.drawLine(2,4,2,28)
+                ticks = 0
+            else
+                c(_[1][1], _[1][2], lerp(_[1][3], _[1][3]+25, ticks/300), 250)
+                screen.drawRectF(3,3,26,26)
+                screen.drawLine(4,2,28,2)
+                screen.drawLine(29,4,29,28)
+                screen.drawLine(4,29,28,29)
+                screen.drawLine(2,4,2,28)
+                if ticks == 300 then
+                    godown = true
+                end
+                if ticks == 0 then
+                    godown = false
+                end
+                if not godown then
+                    ticks = ticks + 1
+                else
+                    ticks = ticks - 1
+                end
+            end
+
+            --- stupid button outlines
+            c(_[1][1]+55, _[1][2]+55, _[1][3]+55, 250)
+            screen.drawLine(3,20,29,20)
+            screen.drawRectF(15,21,2,8)
+
+            screen.drawLine(16,28,26,28)
+            screen.drawLine(26,27,27,27)
+            screen.drawLine(27,26,28,26)
+            screen.drawLine(28,21,28,26)
+
+            screen.drawLine(6,28,15,28)
+            screen.drawLine(5,27,6,27)
+            screen.drawLine(4,26,5,26)
+            screen.drawLine(3,21,3,26)
+            
+            --- text
+            c(_[2][1], _[2][2], _[2][3])
+            screen.drawText(4,4, "Ch:" .. channel)
+
+            --- signal strength bars
+            if signalStrength > 0.9 then
+                screen.drawRectF(4, 15, 4, 4)
+                screen.drawRectF(11, 13, 4, 6)
+                screen.drawRectF(18, 11, 4, 8)
+                screen.drawRectF(25, 9, 4, 10)
+            elseif signalStrength > 0.7 then
+                screen.drawRectF(4, 15, 4, 4)
+                screen.drawRectF(11, 13, 4, 6)
+                screen.drawRectF(18, 11, 4, 8)
+                screen.drawRect(25, 9, 3, 9)
+            elseif signalStrength > 0.5 then
+                screen.drawRectF(4, 15, 4, 4)
+                screen.drawRectF(11, 13, 4, 6)
+                screen.drawRect(18, 11, 3, 7)
+                screen.drawRect(25, 9, 3, 9)
+            elseif signalStrength > 0.3 then
+                screen.drawRectF(4, 15, 4, 4)
+                screen.drawRect(11, 13, 3, 5)
+                screen.drawRect(18, 11, 3, 7)
+                screen.drawRect(25, 9, 3, 9)
+            elseif signalStrength > 0 then
+                c(150, 50, 50)
+                screen.drawRectF(4, 15, 4, 4)
+                screen.drawRect(11, 13, 3, 5)
+                screen.drawRect(18, 11, 3, 7)
+                screen.drawRect(25, 9, 3, 9)
+                c(_[2][1], _[2][2], _[2][3])
+            else
+                screen.drawLine(4, 18, 8, 18)
+                screen.drawLine(11, 18, 15, 18)
+                screen.drawLine(18, 18, 22, 18)
+                screen.drawLine(25, 18, 29, 18)
+            end
+
+            --- up arrow
+            if chup then
+                c(_[3][1], _[3][2], _[3][3])
+                screen.drawLine(9,22,9,27)
+                screen.drawLine(10,23,10,25)
+                screen.drawLine(8,23,8,25)
+                screen.drawRectF(11,24,1,1)
+                screen.drawRectF(7,24,1,1)
+            else
+                c(_[2][1], _[2][2], _[2][3])
+                screen.drawLine(9,22,9,27)
+                screen.drawLine(10,23,10,25)
+                screen.drawLine(8,23,8,25)
+                screen.drawRectF(11,24,1,1)
+                screen.drawRectF(7,24,1,1)
+            end
+
+            --- down arrow
+            if chdown then
+                c(_[3][1], _[3][2], _[3][3])
+                screen.drawLine(22,22,22,27)
+                screen.drawLine(23,24,23,26)
+                screen.drawLine(21,24,21,26)
+                screen.drawRectF(24,24,1,1)
+                screen.drawRectF(20,24,1,1)
+            else
+                c(_[2][1], _[2][2], _[2][3])
+                screen.drawLine(22,22,22,27)
+                screen.drawLine(23,24,23,26)
+                screen.drawLine(21,24,21,26)
+                screen.drawRectF(24,24,1,1)
+                screen.drawRectF(20,24,1,1)
+            end
+        else
             c(_[1][1], _[1][2], _[1][3],250) --i love tables
             screen.drawRectF(3,3,26,26)
             screen.drawLine(4,2,28,2)
             screen.drawLine(29,4,29,28)
             screen.drawLine(4,29,28,29)
             screen.drawLine(2,4,2,28)
-            ticks = 0
-        else
-            c(_[1][1], _[1][2], lerp(_[1][3], _[1][3]+25, ticks/300), 250)
-            screen.drawRectF(3,3,26,26)
-            screen.drawLine(4,2,28,2)
-            screen.drawLine(29,4,29,28)
-            screen.drawLine(4,29,28,29)
-            screen.drawLine(2,4,2,28)
-            if ticks == 300 then
-                godown = true
-            end
-            if ticks == 0 then
-                godown = false
-            end
-            if not godown then
-                ticks = ticks + 1
-            else
-                ticks = ticks - 1
-            end
-        end
-
-        --- stupid button outlines
-        c(_[1][1]+55, _[1][2]+55, _[1][3]+55, 250)
-        screen.drawLine(3,20,29,20)
-        screen.drawRectF(15,21,2,8)
-
-        screen.drawLine(16,28,26,28)
-        screen.drawLine(26,27,27,27)
-        screen.drawLine(27,26,28,26)
-        screen.drawLine(28,21,28,26)
-
-        screen.drawLine(6,28,15,28)
-        screen.drawLine(5,27,6,27)
-        screen.drawLine(4,26,5,26)
-        screen.drawLine(3,21,3,26)
-        
-        --- text
-        c(_[2][1], _[2][2], _[2][3])
-        screen.drawText(4,4, "Ch:" .. channel)
-
-        --- signal strength bars
-        if signalStrength > 0.9 then
-            screen.drawRectF(4, 15, 4, 4)
-            screen.drawRectF(11, 13, 4, 6)
-            screen.drawRectF(18, 11, 4, 8)
-            screen.drawRectF(25, 9, 4, 10)
-        elseif signalStrength > 0.7 then
-            screen.drawRectF(4, 15, 4, 4)
-            screen.drawRectF(11, 13, 4, 6)
-            screen.drawRectF(18, 11, 4, 8)
-            screen.drawRect(25, 9, 3, 9)
-        elseif signalStrength > 0.5 then
-            screen.drawRectF(4, 15, 4, 4)
-            screen.drawRectF(11, 13, 4, 6)
-            screen.drawRect(18, 11, 3, 7)
-            screen.drawRect(25, 9, 3, 9)
-        elseif signalStrength > 0.3 then
-            screen.drawRectF(4, 15, 4, 4)
-            screen.drawRect(11, 13, 3, 5)
-            screen.drawRect(18, 11, 3, 7)
-            screen.drawRect(25, 9, 3, 9)
-        elseif signalStrength > 0 then
-            c(150, 50, 50)
-            screen.drawRectF(4, 15, 4, 4)
-            screen.drawRect(11, 13, 3, 5)
-            screen.drawRect(18, 11, 3, 7)
-            screen.drawRect(25, 9, 3, 9)
-            c(_[2][1], _[2][2], _[2][3])
-        else
-            screen.drawLine(4, 18, 8, 18)
-            screen.drawLine(11, 18, 15, 18)
-            screen.drawLine(18, 18, 22, 18)
-            screen.drawLine(25, 18, 29, 18)
-        end
-
-        --- up arrow
-        if chup then
-            c(_[3][1], _[3][2], _[3][3])
-            screen.drawLine(9,22,9,27)
-            screen.drawLine(10,23,10,25)
-            screen.drawLine(8,23,8,25)
-            screen.drawRectF(11,24,1,1)
-            screen.drawRectF(7,24,1,1)
-        else
-            c(_[2][1], _[2][2], _[2][3])
-            screen.drawLine(9,22,9,27)
-            screen.drawLine(10,23,10,25)
-            screen.drawLine(8,23,8,25)
-            screen.drawRectF(11,24,1,1)
-            screen.drawRectF(7,24,1,1)
-        end
-
-        --- down arrow
-        if chdown then
-            c(_[3][1], _[3][2], _[3][3])
-            screen.drawLine(22,22,22,27)
-            screen.drawLine(23,24,23,26)
-            screen.drawLine(21,24,21,26)
-            screen.drawRectF(24,24,1,1)
-            screen.drawRectF(20,24,1,1)
-        else
-            c(_[2][1], _[2][2], _[2][3])
-            screen.drawLine(22,22,22,27)
-            screen.drawLine(23,24,23,26)
-            screen.drawLine(21,24,21,26)
-            screen.drawRectF(24,24,1,1)
-            screen.drawRectF(20,24,1,1)
+            screen.setColor(100, 100, 100)
+            screen.drawTextBox(4, 4, 28, 28, "comp not connected")
         end
     end
 end
